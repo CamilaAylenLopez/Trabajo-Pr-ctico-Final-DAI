2623449802b242d4ef1a613149016da46af4663d
Gabriel Guivisdalsky <santiago.kasses@gmail.com>
2023-11-08 10:19:40 -0300

    vibracion

--- my-app/navigation/AppNavigator.js
+++ my-app/navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import Contactos from '../screens/Contactos';
+import VibracionScreen from '../screens/Vibracion';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ const AppNavigator = () => {
             <Stack.Navigator>
                 <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Contactos" component={Contactos} />
+                <Stack.Screen name="Vibracion" component={VibracionScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
